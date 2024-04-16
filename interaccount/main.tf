540ea06033b995c5d1ea1a62c33a3eeb246f0318
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-16 19:33:25 +0530

    Terraform

--- interaccount/main.tf
+++ interaccount/main.tf
@@ -5,3 +5,8 @@ provider "aws" {
     role_arn = "arn:aws:iam::211125516612:role/TerraformOUAccess" # ARN of the IAM role in OU account
   }
 }
+
+
+resource "aws_instance" "sample" {
+  ami           = "ami-00d48a21603b2119b"
+  instance_type = "t3.micro"
