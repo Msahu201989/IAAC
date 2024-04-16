10de7f14b8b063ae78723e3b1deb537b9bbe1053
Mukesh.Sahu <msahu201989@gmail.com>
2024-04-16 23:12:56 +0530

    Terraform

--- interaccount/main.tf
+++ interaccount/main.tf
@@ -1,13 +1,6 @@
 provider "aws" {
-  region = "us-east-1" # Change this to your desired region
-
+  region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::211125516612:role/TerraformOUAccess" # ARN of the IAM role in OU account
+    role_arn = "arn:aws:iam::211125516612:role/crossaccountpermission"
   }
-}
-
-
-resource "aws_instance" "sample" {
-  ami           = "ami-00d48a21603b2119b"
-  instance_type = "t3.micro"
 }
